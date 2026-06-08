0a3f03a25b6182de142e87e0bd5ae5252ea14598
coducation <codingmakerone@gmail.com>
2026-06-08 18:24:23 +0900

    style: adjust grid col-spans to move vertical divider and bullet list to the right

--- src/app/logistics/page.tsx
+++ src/app/logistics/page.tsx
@@ -787,7 +787,7 @@ export default function LogisticsPage() {
                   style={{ borderLeftColor: pillar.accentColor }}
                 >
                   {/* Left Column: Category Name */}
-                  <div className="md:col-span-3 flex flex-col justify-center text-left h-full md:border-r md:border-neutral-100 md:pr-6">
+                  <div className="md:col-span-4 flex flex-col justify-center text-left h-full md:border-r md:border-neutral-100 md:pr-6">
                     <h4 className={`text-lg md:text-xl font-black flex items-center gap-2.5 transition-colors duration-300 ${pillar.text}`}>
                       {pillar.title}
                     </h4>
@@ -796,7 +796,7 @@ export default function LogisticsPage() {
                   </div>
 
                   {/* Right Column: Bullets List */}
-                  <div className="md:col-span-9 md:pl-6">
+                  <div className="md:col-span-8 md:pl-8">
                     <ul className="space-y-3.5 text-left">
                       {pillar.bullets.map((bullet, bIdx) => (
                         <li key={bIdx} className="flex items-start gap-3 group/item">
